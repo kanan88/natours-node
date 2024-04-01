8b6b9b474ccb2fc65d41710e37eb8687465b3f64
Kanan Farzali <kanan.farzali@gmail.com>
2024-04-01 14:38:08 +0200

    chaining middlewares and setting up environment variables

--- app.js
+++ app.js
@@ -6,17 +6,18 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1) MIDDLEWARES
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
-// 2) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
