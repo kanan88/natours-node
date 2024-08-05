daac7f5896a8dbbe75b2937bd58d9d587f5b38fc
Kanan Farzali <kanan.farzali@gmail.com>
2024-08-05 13:59:52 +0200

    protectin routes part 1

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const express = require('express');
 const morgan = require('morgan');
 
@@ -17,6 +18,7 @@ app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  console.log(req.headers);
   next();
 });
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -49,3 +49,28 @@ exports.login = catchAsync(async (req, res, next) => {
     token,
   });
 });
+
+exports.protect = catchAsync((req, res, next) => {
+  // 1) Getting token and check if it is there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please login for access.', 401),
+    );
+  }
+
+  // 2) Verification token
+
+  // 3) Check if user still exists
+
+  // 4) Check if user changed password after the token was issued
+
+  next();
+});
