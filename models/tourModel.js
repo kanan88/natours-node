deb33b4ea1e498632c7cc053c8aa899eed5446d3
Kanan Farzali <kanan.farzali@gmail.com>
2024-12-05 20:23:44 +0100

    modelling tour guides: embedding

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -101,6 +102,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -116,6 +118,11 @@ tourSchema.virtual('durationWeeks').get(function () {
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
+});
+
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map((id) => User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
 });
 
 // tourSchema.pre('save', (next) => {
